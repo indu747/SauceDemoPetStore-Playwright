0d7cc28115d5e4140ebb395fe581d5b529e6e030
erik <erik.ostlind@gmail.com>
2024-09-27 02:36:14 +0200

    datafactory and correct tagging

--- tests/petStoreAPI/putPet.api.spec.js
+++ tests/petStoreAPI/putPet.api.spec.js
@@ -1,68 +1,97 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
-const { WebUtils } = require('../../pageObjects/WebUtils.js');
-const { describe } = require('node:test');
-const validStatuses = ["available", "pending", "sold"]
+const validStatuses = ["available", "pending", "sold"];
+import { createPet, deletePet, updatePet, getPet } from "../../helpers/pet.js";
 require('dotenv').config();
+let createdPet;
 
-describe('Tests for PUT /pet', () => {
-    let webUtils;
-    test.beforeEach(async ({ page,request }) => {
-        webUtils = new WebUtils(page)
+test.describe('Tests for PUT /pet',{tag:'@api'}, () => {
+    test.beforeEach(async () => {
         //Creates the pet to modify with PUT request
-        await webUtils.addPet(request)
+        createdPet = await createPet();
+
 
     })
-    test.afterEach(async ({ page,request}) => {
-        webUtils = new WebUtils(page)
-        //Deletes the pet after each test
-        await webUtils.deletePet(request)
+    test.afterEach(async () => {
+        //Deletes the ´created pet after each test
+        await deletePet(createdPet.id)
     })
 
-    test('Send a valid request and make sure all values are updated correctly.', async ({ request }) => {
-        var response;
-        var responseBody;
-        
-        response = await request.put('https://petstore.swagger.io/v2/pet/', { data: webUtils.updatedPet });
-        responseBody = await response.json()
-        expect(response.status()).toBe(200);
-        response = await request.get(`https://petstore.swagger.io/v2/pet/${webUtils.updatedPet.id}`)
-        responseBody = await response.json()
-
-        expect(responseBody).toStrictEqual(webUtils.updatedPet)
-
-    })
-
-    test('Send a request with invalid status, it should not go through or update previous values.', async ({ request }) => {
+    test('Send a valid request and make sure all values are updated correctly.', async () => {
         var response;
         var responseBody;
 
-        response = await request.put('https://petstore.swagger.io/v2/pet/', { data: webUtils.invalidStatus });
+        var modifiedPet = {
+            ...createdPet,
+            category: { id: 1, name: "modifiedCategoryName" },
+            name: "modifiedName",
+            photoUrls: ["testurl.com"],
+            tags: [{ id: 2, name: "female" }],
+            status: "pending"
+        };
+        response = await updatePet(modifiedPet) // Update pet
+        expect(response.status).toBe(200)
+        expect(response.body).toStrictEqual(modifiedPet)
+
+        responseBody = await getPet(createdPet.id) //Get pet by original ID
+        expect(responseBody).toStrictEqual(modifiedPet)  //Assert that pet has been modified as said
+
+    })
+
+    test('Send a request with invalid status, it should return status code 405 Validation Exception.', async () => {
+        var response;
+        var modifiedPet = {
+            ...createdPet,
+            status: "invalidStatus"
+        };
+
+        response = await updatePet(modifiedPet);
         //This should return a validation exception according to documentation
-        expect(response.status()).toBe(405)
-        responseBody = await response.json()
+        expect(response.status).toBe(405)
 
+    })
+
+    test('Send a request with invalid status, status should not be saved.', async () => {
+        var response;
+        var responseBody;
+        var modifiedPet = {
+            ...createdPet,
+            status: "invalid"
+        };
+
+        response = await updatePet(modifiedPet);
         //Get the updated Pet to assert status has allowed value
-        response = await request.get(`https://petstore.swagger.io/v2/pet/${webUtils.updatedPet.id}`)
-        responseBody = await response.json()
-        var valueExistInArray = validStatuses.some(status => status ===responseBody.status)
-        
-        expect(valueExistInArray).toBe(true)
+        response = await getPet(createdPet.id);
+
+        var hasValidStatus = validStatuses.some(status => status === response.status)
+
+        expect(hasValidStatus).toBe(true)
     })
 
-    test('Send a request without a name, it should not go through or update previous values.', async ({ request }) => {
+    test('Send a request without a name, it should return status code 405 Validation Exception.', async () => {
+        var response;
+        var modifiedPet = {
+            ...createdPet,
+        };
+        delete modifiedPet.name;
+        //Update without name field
+        response = await updatePet(modifiedPet)
+        //This should return a validation exception according to documentation
+        expect(response.status).toBe(405)
+    })
+
+    test('Send a request without a name, it should not remove name from pet.', async () => {
         var response;
         var responseBody;
-        //Update without name field
-        response = await request.put('https://petstore.swagger.io/v2/pet/', { data: webUtils.noNamePet });
-        responseBody = await response.json()
-        //This should return a validation exception according to documentation
-        expect(response.status()).toBe(405)
-        //Get the updated Pet to assert pet has a name
-        response = await request.get(`https://petstore.swagger.io/v2/pet/${webUtils.noNamePet.id}`)
-        responseBody = await response.json()
-        //This name should not be altered
-        expect(responseBody.name).toBeDefined()
+        //Send object without name
+        var modifiedPet = {
+            ...createdPet,
+        };
+        delete modifiedPet.name;
+        await updatePet(modifiedPet);
+        //The name should not be removed from pet
+        response = await getPet(modifiedPet.id)
+        expect(response.name).toBeDefined();
 
     })
 })
